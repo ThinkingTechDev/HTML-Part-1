8ed07cd9e83c57781832909072cb617fd9df34c9
nomuk984 <nomundari0804@gmail.com>
2024-11-29 16:45:09 +0800

    all in one

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("keydown", (e) => {
+    if (e.key === "1") {
+        window.location.href = "without.html";
+    } else if (e.key === "2") {
+        window.location.href = "with.html";
+    } else if (e.key === "3") {
+        window.location.href = "index.html";
+    }
+});
